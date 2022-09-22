9924996082b556e329616011b9e34d4bf648aced
faisal-a-n <faisalabdulnazeer@gmail.com>
2022-09-22 18:30:35 +0530

    ENV changes in start file

--- start.sh
+++ start.sh
@@ -4,7 +4,9 @@ set -e
 
 echo "Run DB migrations"
 source /app/config.env
+cat /app/config.env
 
 /app/migrate -path /app/migrations -database "$DB_SOURCE" -verbose up
 
 echo "Start the app"
+exec "$@"
