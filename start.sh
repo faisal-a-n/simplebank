51683cdb388b8707cdfffcbc916e5fedaff548d9
faisal-a-n <faisalabdulnazeer@gmail.com>
2022-09-22 18:23:13 +0530

    removed command forwarding

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@
 set -e
 
 echo "Run DB migrations"
-source config.env
+source /app/config.env
+
 /app/migrate -path /app/migrations -database "$DB_SOURCE" -verbose up
 
 echo "Start the app"
-exec "$@"
